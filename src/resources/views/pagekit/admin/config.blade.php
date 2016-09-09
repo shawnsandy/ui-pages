136538ca2a9294969d9bff8279ae87a5bc966fe2
shawn sandy <shawnsandy@shawns-Mac-mini.local>
2016-09-09 00:07:58 -0400

    [wip]

--- src/resources/views/pagekit/admin/config.blade.php
+++ src/resources/views/pagekit/admin/config.blade.php
@@ -5,19 +5,13 @@
 
 @section('content')
     <div class="container-fluid">
-
         <div class="row section">
             <div class="col-md-12">
                 <div class="dashboard-panel">
                     <div class="dashboard-panel-heading">
                         <i class="material material_info_outline"></i> Custom Panel
                     </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa facilis placeat quam. Aspernatur
-                        beatae commodi corporis debitis deleniti dolorem ex explicabo itaque iure laudantium nesciunt
-                        non, pariatur praesentium quibusdam ratione, repellendus saepe, similique sint vero voluptas.
-                        Consequatur deleniti illum mollitia. Accusantium aliquam animi aut dolor, doloremque officiis
-                        quae quasi repellat.
-                    </p>
+
                 </div>
             </div>
 
