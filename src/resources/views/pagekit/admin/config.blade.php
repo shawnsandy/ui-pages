60f1a205b6fb6cc0c333dbc930a2637bd55f452d
shawn sandy <shawnsandy@shawns-Mac-mini.local>
2016-09-12 01:58:30 -0400

    Updated the admin layout

--- src/resources/views/pagekit/admin/config.blade.php
+++ src/resources/views/pagekit/admin/config.blade.php
@@ -5,11 +5,15 @@
     <div class="container-fluid">
         <div class="row section">
             <div class="col-md-12">
-                <div class="dashboard-panel">
-                    <div class="dashboard-panel-heading">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
                         <i class="material material_info_outline"></i> Custom Panel
                     </div>
-
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur eligendi eos fugit, modi
+                            mollitia nemo non obcaecati, perferendis quidem sequi soluta vel voluptates voluptatum!
+                            Accusantium.</p>
+                    </div>
                 </div>
             </div>
         </div>
