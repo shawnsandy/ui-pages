ad61ae39325abc4e496a71139e590e234066602d
shawn sandy <shawnsandy@shawns-Mac-mini.local>
2016-09-26 08:02:48 -0400

    [wip] markdown link generation

--- src/Classes/PageKit.php
+++ src/Classes/PageKit.php
@@ -29,18 +29,52 @@ class PageKit
     /**
      * @param $markdown
      *
+     * @param null $page
      * @return string
      */
-    public function markdown($markdown)
+    public function markdown($markdown, $page = null)
     {
 
         $file = $markdown . '.md';
 
-        if (!Storage::disk('posts')->exists($file))
+        if (isset($page))
+            $file = $page . '/' . $markdown . '.md';
+
+        if (!Storage::disk('markdown')->exists($file))
             return false;
 
-        $file = Storage::disk('posts')->get($file);
+        $file = Storage::disk('markdown')->get($file);
         return $this->markdown->transform($file);
+
+    }
+
+    public function markdownFiles($dir = null)
+    {
+        return Storage::disk('markdown')->allFiles($dir);
+    }
+
+    /**
+     * @param $file_path
+     * @return mixed
+     */
+    public function markdownLink($file_path)
+    {
+        /**
+         *
+         * split the $file_path into an array
+         * parse the segments of the $array into the data array
+         * if the $array count is > 0 link should contain url params
+         *
+         */
+        
+        $array = explode('/', $file_path);
+        $data['dir'] = $array[0];
+        $data['name'] = trim($array[1], '.md');
+        $data['url'] = '/' . $data['dir'] . '?page=' . $data['name'];
+        $data['display_name'] = ucwords(str_replace_array('_', ' ', $data['name']));
+        $data['link'] = '<a href="' . $data["url"] . '" class="markdown-link">' . $data['display_name'] . '</a>';
+        return $data;
+
     }
 
 }
--- src/Controllers/MarkdownController.php
+++ src/Controllers/MarkdownController.php
@@ -1,6 +1,7 @@
 <?php namespace ShawnSandy\PageKit\Controllers;
 
 
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Storage;
 use ShawnSandy\PageKit\Classes\PageKit;
@@ -26,18 +27,30 @@ class MarkdownController extends Controller
         $this->pagekit = $pageKit;
     }
 
-    public function index(){
+    public function index()
+    {
+        $files = Storage::disk('markdown')->directories();
+        $arr = [];
+        foreach ($files as $file):
+         $arr =   trim($file, '.md');
+        endforeach;
 
-        return view('page::markdown.index');
+        return $arr;
     }
 
     /**
+     * @param $posts
+     * @param Request $request
      * @return string
      */
-    public function show($posts){
+    public function show($posts, Request $request)
+    {
 
         $file = $posts;
         $markdown = $this->pagekit->markdown($file);
+        if($request->has('page'))
+            $markdown = $this->pagekit->markdown($request->page, $posts);
+
         return view('page::markdown.show', compact('markdown'));
 
     }
