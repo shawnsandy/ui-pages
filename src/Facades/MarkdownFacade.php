f4ca5137afd36b21f8d9cd7c7abe6438d6949d3a
shawn sandy <shawnsandy@shawns-Mac-mini.local>
2016-10-16 19:32:01 -0400

    Markdown facade

---
+++ src/Facades/MarkdownFacade.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ShawnSandy\PageKit\Facades;
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * Class MarkdownFacade
+ *
+ * @package \ShawnSandy\PageKit\Facades
+ */
+class MarkdownFacade extends Facade
+{
+
+    /**
+     *
+     */
+    public static function getFacadeAccessor(){
+        return 'MD';
+    }
+
+}
