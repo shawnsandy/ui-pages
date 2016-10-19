5a1da5476a2125d6d53ce074dd1c47dc1f1035f7
shawn sandy <shawnsandy@shawns-Mac-mini.local>
2016-10-19 09:39:45 -0400

    markdown-post view partial

--- src/resources/views/pagekit/markdown/markdown-posts.blade.php
+++ src/resources/views/pagekit/markdown/markdown-posts.blade.php
@@ -1,2 +1,9 @@
-<?php
-
+<div class="markdown-posts">
+    <h2>{!! strip_tags($post['title'])  !!}</h2>
+    <p class="meta"><small>Updated {{ $post['last_modified'] }}</small></p>
+    <p>{{ strip_tags($post['excerpt']) }}</p>
+    <div class="read-more text-right">
+        <a href="{{ $post['url'] }}" class="btn btn-link btn-sm text-capitalize">Read full article</a>
+    </div>
+    <hr>
+</div>
