e53f49a76a7f15c9d27d0aaddad6c4f8979a015c
shawn sandy <shawnsandy@shawns-Mac-mini.local>
2016-10-19 10:37:08 -0400

    Empty view

---
+++ src/resources/views/pagekit/shared/no-content.blade.php
@@ -0,0 +1,5 @@
+<div class="alert alert-info text-center">
+    <h1>OOPS! Well the is embarrassing</h1>
+    <p class="lead">Seems we have not added any content to, please check back soon.</p>
+</div>
+
