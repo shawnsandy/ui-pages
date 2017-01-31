56363c292908f6c90804539b66c0629542319141
shawn sandy <shawnsandy04@gmail.com>
2017-01-31 10:23:22 -0500

    Updates the contact form
    - validate/display errors on
    - display thank you on sucess
    - created a message view

---
+++ src/resources/views/pagekit/partials/messages.blade.php
@@ -0,0 +1,15 @@
+@if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+@if(session('success'))
+    <p class="alert alert-success text-center">
+        {{ session('success') }}
+    </p>
+@endif
