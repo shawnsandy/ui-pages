636d7d8d487f719a966fe765c4a42a3e16c62259
shawnsandy04 <shawnsandy04@gmail.com>
2017-03-28 17:51:21 -0400

    index page copied

---
+++ src/resources/views/pagekit/index-default.blade.php
@@ -0,0 +1,38 @@
+@extends('page::page-layouts.default')
+@section('title', 'Laravel Pages')
+@section('body')
+
+@include('page::shared.nav')
+
+@include('page::shared.header', ["class_name" => "landing bg-5"])
+
+<section class="sub-header text-center">
+    <div class="container">
+        <h1>
+            Just add content...
+        </h1>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est error quae nostrum beatae, iusto accusantium repudiandae accusamus veritatis, voluptatum nesciunt dolorem aspernatur saepe a asperiores.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea doloribus similique officiis laudantium ratione praesentium. Voluptatibus, commodi saepe molestias ea iure optio dignissimos. Non, iusto.
+        </p>
+    </div>
+</section>
+
+@include('page::shared.footer')
+
+@endsection
+
+@push('styles')
+
+<style type="text/css">
+    .logo {
+        display: none;
+    }
+</style>
+@endpush
+@push('scripts')
+<script>
+    $(document).ready(function(){
+        $('.logo').fadeToggle( 5000, "linear" );
+    })
+</script>
+@endpush
